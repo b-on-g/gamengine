48081aa0090538d2742acc0f8ec36deb406528b2
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-22 22:53:40 +0300

    $bog_gamengine_scene: node tree and frame step

---
+++ scene/scene.test.ts
@@ -0,0 +1,65 @@
+namespace $ {
+
+	class $bog_gamengine_scene_time_mock extends $mol_state_time {
+
+		@ $mol_mem
+		static stamp( next = 0 ) {
+			return next
+		}
+
+		static now( precision: number ) {
+			return this.stamp()
+		}
+
+	}
+
+	class $bog_gamengine_scene_mover extends $bog_gamengine_node {
+
+		step( dt: number ) {
+			const pos = this.pos()
+			this.pos( new Float32Array([ pos[ 0 ] + dt, pos[ 1 ], pos[ 2 ] ]) )
+		}
+
+	}
+
+	class $bog_gamengine_scene_named extends $bog_gamengine_node {
+
+		@ $mol_mem
+		kids( next = [] as readonly $bog_gamengine_node[] ) {
+			return next
+		}
+
+	}
+
+	$mol_test({
+
+		'three ticks of 16 ms move node by 0.048'( $ ) {
+			$.$mol_state_time = $bog_gamengine_scene_time_mock
+			const mover = new $bog_gamengine_scene_mover
+			const scene = new $bog_gamengine_scene
+			scene.$ = $
+			scene.kids = ()=> [ mover ]
+			$bog_gamengine_scene_time_mock.stamp( 0 )
+			scene.step()
+			$bog_gamengine_scene_time_mock.stamp( 16 )
+			scene.step()
+			$bog_gamengine_scene_time_mock.stamp( 32 )
+			scene.step()
+			$bog_gamengine_scene_time_mock.stamp( 48 )
+			scene.step()
+			$mol_assert_ok( Math.abs( mover.pos()[ 0 ] - 0.048 ) < 1e-9 )
+		},
+
+		'nodes lists tree depth first with parent before kids'() {
+			const a = new $bog_gamengine_scene_named
+			const b = new $bog_gamengine_scene_named
+			const c = new $bog_gamengine_scene_named
+			a.kids([ b ])
+			const scene = new $bog_gamengine_scene
+			scene.kids = ()=> [ a, c ]
+			$mol_assert_equal( scene.nodes(), [ a, b, c ] )
+		},
+
+	})
+
+}
---
+++ scene/scene.ts
@@ -0,0 +1,41 @@
+namespace $ {
+
+	export class $bog_gamengine_scene extends $bog_gamengine_node {
+
+		@ $mol_mem
+		clock( next?: $bog_gamengine_clock ) {
+			return next ?? new $bog_gamengine_clock
+		}
+
+		@ $mol_mem
+		nodes() {
+			const list = [] as $bog_gamengine_node[]
+			const walk = ( node: $bog_gamengine_node )=> {
+				const kids = node.kids()
+				for( let i = 0; i < kids.length; ++i ) {
+					list.push( kids[ i ] )
+					walk( kids[ i ] )
+				}
+			}
+			walk( this )
+			return list as readonly $bog_gamengine_node[]
+		}
+
+		batches() {
+			return [] as readonly { fill(): number }[]
+		}
+
+		@ $mol_mem
+		step() {
+			const frame = this.clock().frame()
+			const dt = this.clock().dt()
+			const nodes = this.nodes()
+			for( let i = 0; i < nodes.length; ++i ) nodes[ i ].step( dt )
+			const batches = this.batches()
+			for( let i = 0; i < batches.length; ++i ) batches[ i ].fill()
+			return frame
+		}
+
+	}
+
+}
