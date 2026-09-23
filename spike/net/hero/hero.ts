1b9a77df087e91371c64b81b169a05fcb6400d9d
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-23 15:33:48 +0300

    $bog_gamengine: net spike into architecture, spike dropped

--- spike/net/hero/hero.ts
+++
@@ -1,8 +0,0 @@
-namespace $ {
-
-	export class $bog_gamengine_spike_net_hero extends $giper_baza_dict.with({
-		X: $giper_baza_atom_real,
-		Y: $giper_baza_atom_real,
-	}) {}
-
-}
--- spike/net/world/world.ts
+++
@@ -1,7 +0,0 @@
-namespace $ {
-
-	export class $bog_gamengine_spike_net_world extends $giper_baza_dict.with({
-		Heroes: $giper_baza_dict_to( $bog_gamengine_spike_net_hero ),
-	}) {}
-
-}
